955efeac6b24168bd565940b41843e4db0da9b28
sofia-mancini <sofia.mancini@uri.edu>
2026-03-25 11:17:47 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,44 +1,18 @@
 <?php
 
-  /* TO-DO: Include database-connection.php to connect to the database
-          Hint: Use require_once to ensure the file is only loaded once.
-                Load this before any redirects or login logic.
-                Both header.php and database-connection.php are inside the includes folder
-  */
+  require_once 'inclues/database-connection.php';
+  require_once 'includes/session.php';
 
-
-
-  /* TO-DO: Include session.php to handle login sessions
-          Hint: Use require_once to avoid redeclaring functions if the file is loaded elsewhere.
-                Load this before any redirects or login logic.
-                Both header.php and session.php are inside the includes folder
-  */
-
-
-
-  
   if ($logged_in) {                                       // If already logged in  
     header('Location: profile.php');                     // Redirect to profile page 
     exit;                                               // Stop further code running
   }    
 
-
-
   if ($_SERVER["REQUEST_METHOD"] == "POST") {         // Check if the form was submitted
     $username = $_POST['username'];                  // Get the username the user sent
     $password = $_POST['password'];                 // Get the password the user sent
 
-
-
-    /* TO-DO: Call authenticate() function to verify the username and password
-              Pass the appropriate arguments (hint: use variables given above)
-              Store the returned value in a variable called $user
-
-              Hint: You defined authenticate() earlier in session.php
-    */
-
-
-
+    $user = authenticate($pdo, $username, $password);
     if ($user) {                               // If user data returned
       login($user);                           // Call the login function to update session data                                             
       header('Location: profile.php');       // Redirect to profile page 
@@ -46,12 +20,8 @@
     }
   }
 
-    
-  /* TO-DO: Include header.php
-            Hint: Include this AFTER redirects so that HTML output does not break header() functions.
-                  header.php is inside the includes folder and already connects to the database
-  */
-    
+  include 'includes/header.php';
+  
 ?> 
 
 <div id="content" class="login-container animate-bottom">
