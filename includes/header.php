bdfef4c1b87c6598bd38664c1906d43b83c2492a
sofiamancini <sofia.mancini@uri.edu>
2026-03-25 20:31:00 -0400

    h

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <?php
-  require_once __DIR__ . '/database-connection.php';
-  require_once __DIR__ . '/session.php';
+  require_once 'includes/database-connection.php';
+  require_once 'includes/session.php';
 ?>
 
 <!DOCTYPE html>
